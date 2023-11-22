bc17717b462b378b5a1b34ce4c14a98f972826ac
raziqaizza <91117113+raziqaizza@users.noreply.github.com>
2023-11-22 22:00:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projek/Home.java
@@ -0,0 +1,35 @@
+package com.example.projek;
+
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.example.projek.databinding.HomeBinding;
+
+public class Home extends AppCompatActivity {
+    HomeBinding binding;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = HomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.idLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                finish();
+            }
+        });
+    }
+
+
+
+}
